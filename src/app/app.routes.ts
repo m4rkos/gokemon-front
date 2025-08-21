f8a52e7688ab90c919c3febc26af90590feef557
marcos eduardo <marcos.j.eduardo@gmail.com>
2025-08-21 22:02:30 +0100

    done last route adjust

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 import { CatalogComponent } from './pages/catalog/catalog.component';
 
 export const routes: Routes = [
-    {path: "", component: HomeComponent},
-    {path: "catalog", component: CatalogComponent},
-    {path: "catalog/:id", component: CatalogComponent},
+    {path: "", component: CatalogComponent},
+    {path: ":id", component: CatalogComponent},
 ];
